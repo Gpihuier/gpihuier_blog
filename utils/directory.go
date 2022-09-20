d5d10b6d2d0aaf77d13d908e4404db6606e53bb0
guihui <414258965@qq.com>
2022-09-20 14:13:39 +0800

    fix: add utils

--- utils/directory.go
+++ utils/directory.go
@@ -21,6 +21,22 @@ func PathExists(path string) (bool, error) {
 	return false, err
 }
 
+// CreateDir 批量创建文件夹
+func CreateDir(dirs ...string) error {
+	for _, v := range dirs {
+		exist, err := PathExists(v)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			if err = os.MkdirAll(v, os.ModePerm); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 func GetRootPath() string {
 	rootPath, _ := os.Getwd()
 	if strings.HasSuffix(rootPath, "cmd") {
@@ -28,4 +44,50 @@ func GetRootPath() string {
 
 	}
 	return rootPath
+}
+
+// Exist 检查文件或目录是否存在
+// 如果由 filename 指定的文件或目录存在则返回 true，否则返回 false
+func Exist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+// ScanDir 列出指定路径中的文件和目录
+// 如果目录不存在，则返回空slice
+func ScanDir(directory string) []string {
+	file, err := os.Open(directory)
+	if err != nil {
+		return []string{}
+	}
+	names, err := file.Readdirnames(-1)
+	if err != nil {
+		return []string{}
+	}
+	return names
+}
+
+// IsDir 判断给定文件名是否是一个目录
+// 如果文件名存在并且为目录则返回 true。如果 filename 是一个相对路径，则按照当前工作目录检查其相对路径。
+func IsDir(filename string) bool {
+	return isFileOrDir(filename, true)
+}
+
+// IsFile 判断给定文件名是否为一个正常的文件
+// 如果文件存在且为正常的文件则返回 true
+func IsFile(filename string) bool {
+	return isFileOrDir(filename, false)
+}
+
+// 判断是文件还是目录，根据decideDir为true表示判断是否为目录；否则判断是否为文件
+func isFileOrDir(filename string, decideDir bool) bool {
+	fileInfo, err := os.Stat(filename)
+	if err != nil {
+		return false
+	}
+	isDir := fileInfo.IsDir()
+	if decideDir {
+		return isDir
+	}
+	return !isDir
 }
