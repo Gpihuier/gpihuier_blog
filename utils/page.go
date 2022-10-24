d50c72881a87992610fdffb8b0a058c0f383ca5f
guihui <414258965@qq.com>
2022-10-24 15:01:58 +0800

    feat: add gorm paginate

--- utils/page.go
+++ utils/page.go
@@ -1,8 +1,37 @@
 package utils
 
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
 // GetPageData 获取分页数据
 func GetPageData(page, pageSize int) (limit, offset int) {
 	limit = pageSize
 	offset = pageSize * (page - 1)
 	return
+}
+
+// Paginate Gorm 分页 https://gorm.cn/zh_CN/docs/scopes.html#pagination
+// example: db.Scopes(Paginate(c)).Find(&users)
+func Paginate(c *gin.Context) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page, _ := strconv.Atoi(c.Query("page"))
+		if page == 0 {
+			page = 1
+		}
+
+		pageSize, _ := strconv.Atoi(c.Query("page_size"))
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
 }
